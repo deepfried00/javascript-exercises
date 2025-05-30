fe40776353de77e286b38a24c3c97aaaa3d05486
deepfried00 <zarintasnim0770@gmail.com>
2025-05-30 17:31:03 +0600

    finished 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if ( num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    if ( num1 < 0 || num2 < 0 || num1 % 1 !== 0 || num2 % 1 !== 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    };
+    return sum;
 };
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
