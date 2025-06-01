d0c9caa70b5244077cf96f16b172bf386194659a
deepfried00 <zarintasnim0770@gmail.com>
2025-06-01 20:47:30 +0600

    done with 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,40 @@
-const add = function() {
+const add = function(num1 , num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1 , num2) {
+	return num1 - num2;
+};
+
+const sum = function(array) {
+  return array.reduce((acc, item) => {
+    return acc + item; 
+  }, 0);
+};
+
+const multiply = function(array) {
+return array.reduce((acc, item) => {
+  return acc * item;
+}, 1)
+
+};
+
+const power = function(num1 , num2) {
+  return Math.pow(num1, num2);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  let fact = num;
+  if (num == 0){
+    return 1;
+  }
+  else {
+    for (let i = 1; i < num; i++){
+      fact *= i;
+    }
+    return fact;
+  }
 	
 };
 
