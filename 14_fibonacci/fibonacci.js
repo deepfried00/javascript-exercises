b8f6564b75fc56e2a113a2e578db60e423299184
deepfried00 <zarintasnim0770@gmail.com>
2025-06-02 01:23:12 +0600

    done with 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 1 || n == 0){
+        return parseInt(n);
+    }
+    if (n < 0){
+        return "OOPS";
+    }
+    return fibonacci(n - 2) + fibonacci(n - 1);
 
 };
 
