f39eed2ee6f96f5077cc42828749f3d2cf8c9568
deepfried00 <zarintasnim0770@gmail.com>
2025-06-01 22:12:00 +0600

    done with 1

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const given = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}\s~]/g, "").toLowerCase();
+    console.log(given);
+    const reverse = given.split("").reverse().join("");
+    console.log(reverse);
+    if (given === reverse) {
+        return true;
+    }
+    return false;
 };
+console.log(palindromes('r3ace3car'));
+
 
 // Do not edit below this line
 module.exports = palindromes;
