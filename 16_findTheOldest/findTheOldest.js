396c3bba95f659e555b2083eb4d88fbdc7e6651e
deepfried00 <zarintasnim0770@gmail.com>
2025-06-02 02:58:38 +0600

    DONE WITH 16git add .

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let data = obj.reduce((accumulator , currPerson) => {
+        let deathYear = currPerson.yearOfDeath || new Date().getFullYear();
+        const age = deathYear - currPerson.yearOfBirth;
+        accumulator[currPerson.name] = age;
+        return accumulator;
+    }, {}
+    );
 
+    const maxAge = Math.max(...Object.values(data));
+    const oldName = Object.keys(data).find(name => data[name] === maxAge);
+    return obj.find(person => person.name === oldName );
 };
 
+
+//     const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//     ]
+
+// console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
