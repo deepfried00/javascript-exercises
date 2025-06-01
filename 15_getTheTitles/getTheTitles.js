f136353473fb1223680fff22a141ccf5bd23408c
deepfried00 <zarintasnim0770@gmail.com>
2025-06-02 01:44:44 +0600

    done with 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let titles = obj.map((obj) =>  obj.title );
+    return titles ;
+    
 };
 
 // Do not edit below this line
